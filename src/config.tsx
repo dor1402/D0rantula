3afcdc1c151d2aaed0dd4c4842c3dec600dd328f
dorantula <dorantula.dvir.raviv@gmail.com>
2021-12-18 02:54:50 +0200

    crucial bug fix

--- src/config.tsx
+++ src/config.tsx
@@ -19,7 +19,7 @@ const config = {
 
     description: [
         'Hello! my name is Dor and I am a Full Stack Web Developer :)',
-        'I use are NextJS, Python, Node, c# and both relational/non relational databases.',
+        'I use NextJS, Python, Node, c# and both relational/non relational databases.',
         'I am also familiar with both Windows and Linux operating systems.',
         'Here are some of the project I have worked on, enjoy!'
     ],
